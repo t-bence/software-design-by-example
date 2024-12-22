f5b3644b9d6b0ba28ebc829ebfd241be03434fcf
t-bence <toth.bence.mihaly@gmail.com>
2024-12-22 19:35:57 +0100

    Matching patterns code from book

---
+++ 4_matching_patterns/patterns.py
@@ -0,0 +1,56 @@
+from abc import ABC
+
+class Match(ABC):
+    def __init__(self, rest):
+        self.rest = rest if rest is not None else Null()
+    
+    def match(self, text):
+        result = self._match(text, 0)
+        return result == len(text)
+
+    def _match(self, text, start):
+        raise NotImplementedError()
+
+class Lit(Match):
+    def __init__(self, chars, rest=None):
+        super().__init__(rest)
+        self.chars = chars
+
+    def _match(self, text, start):
+        end = start + len(self.chars)
+        if text[start:end] != self.chars:
+            return None
+        return self.rest._match(text, end)
+
+
+class Any(Match):
+    def __init__(self, rest=None):
+        super().__init__(rest)
+
+    def _match(self, text, start):
+        for i in range(start, len(text) + 1):
+            end = self.rest._match(text, i)
+            if end == len(text):
+                return end
+        return None
+    
+class Either(Match):
+    def __init__(self, left: Lit, right: Lit, rest=None):
+        super().__init__(rest)
+        self.left = left
+        self.right = right
+
+    def _match(self, text: str, start:int=0):
+        for side in (self.left, self.right):
+            end = side._match(text, start)
+            if end is not None:
+                return self.rest._match(text, end)
+        return None
+
+
+class Null(Match):
+    def __init__(self):
+        self.rest = None
+
+    def _match(self, text, start):
+        return start
---
+++ 4_matching_patterns/tests/test_matching.py
@@ -0,0 +1,38 @@
+from patterns import Lit, Any, Either
+
+def test_lit():
+    assert Lit("abc").match("abc")
+
+    assert not Lit("abc").match("def")
+    
+    matcher = Lit("abc", Lit("def"))
+    assert matcher.match("abcdef")
+
+    # pytest.raises(Lit("abc").match, "def")
+
+def test_any():
+    assert Any().match("abc")
+    assert Any().match("")
+
+    assert Any(Lit("abc")).match("abc")
+    assert Any(Lit("abc")).match("xyzabc")
+
+def test_either_two_literals_first():
+    # /{a,b}/ matches "a"
+    assert Either(Lit("a"), Lit("b")).match("a")
+
+def test_either_two_literals_not_both():
+    # /{a,b}/ doesn't match "ab"
+    assert not Either(Lit("a"), Lit("b")).match("ab")
+
+def test_either_followed_by_literal_match():
+    # /{a,b}c/ matches "ac"
+    assert Either(Lit("a"), Lit("b"), Lit("c")).match("ac")
+
+def test_either_followed_by_literal_no_match():
+    # /{a,b}c/ doesn't match "ax"
+    assert not Either(Lit("a"), Lit("b"), Lit("c")).match("ax")
+
+def test_either_followed_by_literal_matches():
+    # /{a,b}c{a,b}/ matches "aca"
+    assert Either(Lit("a"), Lit("b"), Lit("c", Either(Lit("a"), Lit("b")))).match("aca")
